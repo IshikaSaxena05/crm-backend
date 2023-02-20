cd16ab44c07063578aa04f064f917dc774e50c70
vibhupuri3005 <vibhu.puri@orientaloutsourcing.com>
2023-02-20 11:19:21 +0530

    index.js

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
-var cors = require('cors')
-var bodyParser = require('body-parser')
-require("./src/connection/databaseConnection")
-const dotenv = require("dotenv")
-dotenv.config()
-const PORT = process.env.PORT||5000
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const connectDB = require("./src/connection/databaseConnection")
+const web = require("./src/routes/index")
+
+const PORT = process.env.PORT || 5000
+const DB_URL = process.env.DB_URL || "mongodb+srv://cluster0.fdr4btl.mongodb.net/crm"
+
+// database connection
+connectDB(DB_URL);
+
 app.use(express.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// Load routing
+app.use('/',web);
+
 app.listen(PORT,()=>{
     console.log(`Running at ${PORT}`);
 })
